db9def98b3372ac464f15f2983cea81dc8faa20f
Alex Popov <alex.popov@qplaze.com>
2019-12-25 18:22:08 +0200

    init commit, create movement sccript

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 6.0f;
+	public float turnSpeed = 3.0f;
+
+	private CharacterController charController;
+	private RaycastHit lookAtHit;
+
+	void Start()
+	{
+		charController = GetComponent<CharacterController>();
+	}
+	public void LookAt(Vector3 forward)
+	{
+		forward.y = 0;
+		forward.Normalize();
+
+		transform.rotation = Quaternion.Lerp(
+			transform.rotation,
+			Quaternion.LookRotation(forward, Vector3.up),
+			1 - Mathf.Exp(-turnSpeed * Time.deltaTime)
+			);
+		return;
+	}
+
+	public void Move(Vector3 movement)
+	{
+		movement *= speed;
+
+		movement = Vector3.ClampMagnitude(movement, speed);
+		movement *= Time.deltaTime;
+		charController.Move(movement);
+
+	}
+
+}
---
+++ Assets/Scripts/UserPlayerControlInput.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class UserPlayerControlInput : MonoBehaviour
+{
+	private PlayerController controller;
+	private void Start()
+	{
+		controller = GetComponent<PlayerController>();
+	}
+	private void Update()
+	{
+
+		float deltaX = Input.GetAxis("Horizontal");
+		float deltaZ = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+
+
+		if (movement != Vector3.zero)
+		{
+			controller.Move(movement);
+		}
+
+
+		var cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		Vector3 pointOnPlane;
+		Plane horizontalPlane = new Plane(Vector3.up, Vector3.zero);
+		if (RayPlaneIntersection(cameraRay, horizontalPlane, out pointOnPlane))
+		{
+			var forward = pointOnPlane - transform.position;
+			controller.LookAt(forward);
+		}
+
+
+	}
+
+	private bool RayPlaneIntersection(Ray ray, Plane plane, out Vector3 hitPoint)
+	{
+		float intesectionDistance;
+		hitPoint = Vector3.positiveInfinity;
+
+		if(plane.Raycast(ray, out intesectionDistance))
+		{
+			hitPoint = ray.GetPoint(intesectionDistance);
+			return true;
+		}
+		return false;
+	}
+}
